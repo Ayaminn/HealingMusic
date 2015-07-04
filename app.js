1f051921147ed3d3ac378b6a422bebe3f46eee80
Daichi Hirota <flere210_fortis931@yahoo.co.jp>
2015-07-04 15:23:39 +0900

    make it better

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ var server = http.createServer(function(req, res) {
   var pathname = url.parse(req.url).pathname;
   if(pathname === "/"){
   res.writeHead(200, {"Content-Type":"text/html"});
-  var output = fs.readFileSync("test.html", "utf-8");
+  var output = fs.readFileSync("child.html", "utf-8");
+  res.end(output);
+  }
+  if(pathname === "/server"){
+  res.writeHead(200, {"Content-Type":"text/html"});
+  var output = fs.readFileSync("parent.html", "utf-8");
   res.end(output);
   }
 // (見つからなかったら status: 404 で NotFound用のhtmlを返す)
@@ -22,17 +27,10 @@ var io = socketio.listen(server);
 io.sockets.on("connection", function (socket) {
   // メッセージ送信（送信者にも送られる）
   socket.on("C_2_S", function (data) {
-  io.sockets.emit("S_2_C", {value:data.value});
-  console.log("S_2_C", {value:data.value});
-});
+    io.sockets.emit("S_2_C", {value:data.value});
+    console.log("S_2_C", {value:data.value});
+  });
 
-/*
-  // メッセージ送信（送信者にも送られる）
-  socket.on("C_2_S", function (data) {
-  io.sockets.emit("S_2_C", {value:data.value});
-  console.log("S_2_C", {value:data.value});
-});
-*/
 
   // 切断したときに送信
   socket.on("disconnect", function () {
