b05cff8b5b51fc8c8d42b86a16ee7288d72459b6
flere210_fortis931@yahoo.co.jp <flere210_fortis931@yahoo.co.jp>
2015-08-23 05:45:42 +0900

    inishal commit

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ var server = http.createServer(function(req, res) {
   var pathname = url.parse(req.url).pathname;
   if(pathname === "/"){
   res.writeHead(200, {"Content-Type":"text/html"});
-  var output = fs.readFileSync("child.html", "utf-8");
+  var output = fs.readFileSync("Live.html", "utf-8");
   res.end(output);
   }
-  if(pathname === "/server"){
+  if(pathname === "/master"){
   res.writeHead(200, {"Content-Type":"text/html"});
-  var output = fs.readFileSync("parent.html", "utf-8");
+  var output = fs.readFileSync("LiveController.html", "utf-8");
   res.end(output);
   }
 // (見つからなかったら status: 404 で NotFound用のhtmlを返す)
 
 }).listen(process.env.PORT || 3000);
-
+ 
 var io = socketio.listen(server);
 
 io.sockets.on("connection", function (socket) {
   // メッセージ送信（送信者にも送られる）
-  socket.on("C_2_S", function (data) {
-    io.sockets.emit("S_2_C", {value:data.value});
-    console.log("S_2_C", {value:data.value});
-  });
 
+  socket.on("LiveStart", function (data) {
+    io.sockets.emit("S2C", {value:data.value});
+    console.log("動いたよ〜");
+  });
 
   // 切断したときに送信
   socket.on("disconnect", function () {
